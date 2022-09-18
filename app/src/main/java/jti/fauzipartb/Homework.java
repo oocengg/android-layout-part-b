01e495f6824c509bf2585017b4825faaae9b7325
oocengg <fauzinugraha06@gmail.com>
2022-09-18 21:43:43 +0700

    The Layout Editor

---
+++ app/src/main/java/jti/fauzipartb/Homework.java
@@ -0,0 +1,39 @@
+package jti.fauzipartb;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+public class Homework extends AppCompatActivity {
+    private int mCount;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homework);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+
+    public void zeroCount(View view) {
+        mCount=0;
+        if (mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
